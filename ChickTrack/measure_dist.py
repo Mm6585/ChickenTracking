ab8f169c615da8463cf55b7617abd2feb0fcbd13
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-11-07 00:02:25 +0900

    update

--- ChickTrack/measure_dist.py
+++ ChickTrack/measure_dist.py
@@ -1,5 +1,15 @@
 import math
 
-def measure_dist(x1, y1, x2, y2):
-    distance = math.sqrt(math.pow((x1-x2), 2) + math.pow((y1 - y2), 2))
+def measure_dist(x1, y1, x2, y2, box1, box2):
+    coor_dist = math.sqrt(math.pow((x1-x2), 2) + math.pow((y1 - y2), 2))
+    box_dist = max(box1, box2) / min(box1, box2)
+    box_dist = 1
+
+    if (coor_dist == 0):
+        distance = box_dist
+    elif (box_dist == 0):
+        distance = coor_dist
+    else:
+        distance = coor_dist * box_dist
+
     return distance
